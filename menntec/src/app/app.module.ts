275a9e83054f2c383f2177a0846f9c64dc0ad48d
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-02-14 21:09:43 +0100

    component setup

--- menntec/src/app/app.module.ts
+++ menntec/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
+import { DatasecComponent } from './pages/datasec/datasec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    HeroComponent,
+    ProductsComponent,
+    ContactComponent,
+    AboutComponent,
+    ImprintComponent,
+    DatasecComponent
   ],
   imports: [
     BrowserModule,
---
+++ menntec/src/app/pages/datasec/datasec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datasec',
+  templateUrl: './datasec.component.html',
+  styleUrls: ['./datasec.component.sass']
+})
+export class DatasecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
