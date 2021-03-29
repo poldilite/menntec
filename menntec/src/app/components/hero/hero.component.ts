d604ce1134cbd4fc00acf4817b5370d4ea42fdbf
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-03-29 06:25:00 +0200

    added loading

--- menntec/src/app/components/hero/hero.component.ts
+++ menntec/src/app/components/hero/hero.component.ts
@@ -1,8 +1,5 @@
-import { Subscription } from 'rxjs';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { HOMEPAGE_DATA_QUERY } from 'src/app/apollo/queries';
-import { CONTACTS_QUERY } from '../../apollo/queries';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
@@ -10,45 +7,26 @@ import { CONTACTS_QUERY } from '../../apollo/queries';
   styleUrls: ['./hero.component.sass'],
 })
 export class HeroComponent implements OnInit {
-  private hpQuery: Subscription = new Subscription();
+  @Input()
+  heroText!: string;
 
-  data: any = {};
-  loading = false;
-  errors: any;
-  heroText = '';
   heroTextSpan = '';
   heroTextNormal = '';
-  descriptionText = '';
 
-  constructor(private apollo: Apollo) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.loading = true;
-    this.hpQuery = this.apollo
-      .watchQuery({
-        query: HOMEPAGE_DATA_QUERY,
-      })
-      // tslint:disable-next-line: deprecation
-      .valueChanges.subscribe((result) => {
-        this.data = result.data;
-        this.loading = result.loading;
-        this.errors = result.errors;
+    const splitNumber = this.getPosition(this.heroText, ' ', 4);
 
-        this.heroText = this.data.homepage.heroText;
-        const splitNumber = this.getPosition(this.heroText, ' ', 4);
-
-        this.heroTextSpan = this.data.homepage.heroText.slice(0, splitNumber); // TODO: Berechnung anpassen
-        this.heroTextNormal = this.data.homepage.heroText.slice(
-          splitNumber + 1,
-          this.heroText.length
-        );
-      });
+    this.heroTextSpan = this.heroText.slice(0, splitNumber); // TODO: Berechnung anpassen
+    this.heroTextNormal = this.heroText.slice(
+      splitNumber + 1,
+      this.heroText.length
+    );
   }
 
   // tslint:disable-next-line: use-lifecycle-interface
-  ngOnDestroy(): void {
-    this.hpQuery.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 
   getPosition(text: string, subString: string, index: number): number {
     return text.split(subString, index).join(subString).length;
