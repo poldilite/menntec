6b3b5d09f2230367dd19453e8309fa2a082b425d
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-04-07 22:39:26 +0200

    ham nav animation

--- menntec/src/app/components/navigation/navigation.component.ts
+++ menntec/src/app/components/navigation/navigation.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.sass']
+  styleUrls: ['./navigation.component.sass'],
 })
 export class NavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
