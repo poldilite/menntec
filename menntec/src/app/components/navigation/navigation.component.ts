a5d030c9addb0e639080fd2393b140c30a07f754
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-04-10 12:59:15 +0200

    changed to angular animation

--- menntec/src/app/components/navigation/navigation.component.ts
+++ menntec/src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  state,
+  trigger,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.sass'],
+  animations: [
+    trigger('slide', [
+      state(
+        'closed',
+        style({
+          transform: 'translateX(100%)',
+        })
+      ),
+      state(
+        'open',
+        style({
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('open => closed', animate('80ms')),
+      transition('closed => open', animate('250ms')),
+    ]),
+  ],
 })
 export class NavigationComponent implements OnInit {
   constructor() {}
