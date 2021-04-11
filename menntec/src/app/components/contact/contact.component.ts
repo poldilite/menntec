91d5c228111b6a8ebacea5b769647dadc189a1b8
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-04-11 22:44:59 +0200

    contact form with connect to strapi

--- menntec/src/app/components/contact/contact.component.ts
+++ menntec/src/app/components/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Apollo } from 'apollo-angular';
+import { INQUIRIES_MUTATION } from '../../apollo/queries';
 
 @Component({
   selector: 'app-contact',
@@ -9,14 +12,43 @@ export class ContactComponent implements OnInit {
   submitted = false;
   contactData: any = {};
 
-  constructor() {}
+  constructor(private apollo: Apollo) {}
 
   ngOnInit(): void {}
 
   // Submit Contact Form
 
-  onSubmit() {
+  // tslint:disable-next-line: typedef
+  onSubmit(form: NgForm) {
     this.submitted = true;
-    console.log(this.contactData);
+
+    this.apollo
+      .mutate({
+        mutation: INQUIRIES_MUTATION,
+        variables: {
+          firstName: this.contactData.firstName,
+          lastName: this.contactData.lastName,
+          phone: this.contactData.tel,
+          email: this.contactData.eMail,
+          message: this.contactData.message,
+        },
+      })
+      // tslint:disable-next-line: deprecation
+      .subscribe(
+        ({ data }) => {},
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    form.reset();
+    this.resetSubmit();
+  }
+
+  // tslint:disable-next-line: typedef
+  resetSubmit() {
+    setTimeout(() => {
+      this.submitted = false;
+    }, 5000);
   }
 }
