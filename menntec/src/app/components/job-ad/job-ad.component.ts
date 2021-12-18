a496869865efaf39acbd3e437fe59613e4c29905
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-12-18 21:56:34 +0100

    change of slider

--- menntec/src/app/components/job-ad/job-ad.component.ts
+++ menntec/src/app/components/job-ad/job-ad.component.ts
@@ -12,25 +12,26 @@ export class JobAdComponent implements OnInit {
 
   jobAdArray: any = [
     {
-      img: 'https://images.unsplash.com/photo-1604537372111-68af9affc2aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2338&q=80',
-      name: 'Peter',
-      date: '30.11.2021',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1576919228236-a097c32a5cd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-      name: 'Karl',
-      date: '11.11.1911',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80',
-      name: 'Günni',
-      date: '01.01.2021',
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+      title: 'Anlagenmechaniker (m/w/d)',
+      subTitle: 'für Sanitär, Heizung und Klimatechnik',
+      fieldOfActivity: [
+        'Installation von Neubauten',
+        'Modernisierung von Altbauten',
+        'Installation und Instandhaltung von technischen Anlagen in der Kleinindustrie sowie öffentlichen Gebäuden',
+      ],
+      whatToBring: [
+        'Führerschein Klasse B',
+        'Stets freundlicher Umgang mit unseren Kunden',
+        'Zuverlässigkeit, Teamfähigkeit und ein hohes Maß an Leistungsbereitschaft',
+      ],
+      whatToExpect: [
+        'Eine familiäre Arbeitsatmosphäre in einem sympathische Team',
+        'Spannendes Aufgabenspektrum in einem inhabergeführten Betrieb',
+        'Eigenverantwortung für ihr Aufgabengebiet',
+        'Fachliche und persönliche Entwicklungsmöglichkeiten',
+        'Leistungsgerechte Vergütung',
+        'Festanstellung in Vollzeit',
+      ],
     },
   ];
 
@@ -41,16 +42,16 @@ export class JobAdComponent implements OnInit {
   next() {
     if (this.j != 0) {
       this.j--;
-      this.i = this.i - 200;
-      this.slide.nativeElement.style.top = this.i + 'px';
+      this.i = this.i - 75;
+      this.slide.nativeElement.style.top = this.i + 'vh';
     }
   }
 
   prev() {
     if (this.j != this.jobAdArray.length - 1) {
       this.j++;
-      this.i = this.i + 200;
-      this.slide.nativeElement.style.top = this.i + 'px';
+      this.i = this.i + 75;
+      this.slide.nativeElement.style.top = this.i + 'vh';
     }
   }
 }
