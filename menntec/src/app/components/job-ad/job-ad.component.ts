f8542e105131f6ba0501d9ec2baf1e22496656e0
martin olligschläger <m.olligschlaeger@blueberrymedia.de>
2021-12-19 14:35:01 +0100

    desktop ready design

--- menntec/src/app/components/job-ad/job-ad.component.ts
+++ menntec/src/app/components/job-ad/job-ad.component.ts
@@ -33,6 +33,50 @@ export class JobAdComponent implements OnInit {
         'Festanstellung in Vollzeit',
       ],
     },
+    {
+      title: 'Meister (m/w/d)',
+      subTitle: 'für Sanitär, Heizung und Klimatechnik',
+      fieldOfActivity: [
+        'Installation von Neubauten',
+        'Modernisierung von Altbauten',
+        'Installation und Instandhaltung von technischen Anlagen in der Kleinindustrie sowie öffentlichen Gebäuden',
+      ],
+      whatToBring: [
+        'Führerschein Klasse B',
+        'Stets freundlicher Umgang mit unseren Kunden',
+        'Zuverlässigkeit, Teamfähigkeit und ein hohes Maß an Leistungsbereitschaft',
+      ],
+      whatToExpect: [
+        'Eine familiäre Arbeitsatmosphäre in einem sympathische Team',
+        'Spannendes Aufgabenspektrum in einem inhabergeführten Betrieb',
+        'Eigenverantwortung für ihr Aufgabengebiet',
+        'Fachliche und persönliche Entwicklungsmöglichkeiten',
+        'Leistungsgerechte Vergütung',
+        'Festanstellung in Vollzeit',
+      ],
+    },
+    {
+      title: 'Angehender Meister (m/w/d)',
+      subTitle: 'für Sanitär, Heizung und Klimatechnik',
+      fieldOfActivity: [
+        'Installation von Neubauten',
+        'Modernisierung von Altbauten',
+        'Installation und Instandhaltung von technischen Anlagen in der Kleinindustrie sowie öffentlichen Gebäuden',
+      ],
+      whatToBring: [
+        'Führerschein Klasse B',
+        'Stets freundlicher Umgang mit unseren Kunden',
+        'Zuverlässigkeit, Teamfähigkeit und ein hohes Maß an Leistungsbereitschaft',
+      ],
+      whatToExpect: [
+        'Eine familiäre Arbeitsatmosphäre in einem sympathische Team',
+        'Spannendes Aufgabenspektrum in einem inhabergeführten Betrieb',
+        'Eigenverantwortung für ihr Aufgabengebiet',
+        'Fachliche und persönliche Entwicklungsmöglichkeiten',
+        'Leistungsgerechte Vergütung',
+        'Festanstellung in Vollzeit',
+      ],
+    },
   ];
 
   @ViewChild('slide') slide: any;
@@ -40,18 +84,20 @@ export class JobAdComponent implements OnInit {
   i = 0;
 
   next() {
-    if (this.j != 0) {
+    if (this.j !== 0) {
       this.j--;
       this.i = this.i - 75;
       this.slide.nativeElement.style.top = this.i + 'vh';
     }
+    console.log(this.j);
   }
 
   prev() {
-    if (this.j != this.jobAdArray.length - 1) {
+    if (this.j !== this.jobAdArray.length - 1) {
       this.j++;
       this.i = this.i + 75;
       this.slide.nativeElement.style.top = this.i + 'vh';
     }
+    console.log(this.i & this.jobAdArray.length);
   }
 }
